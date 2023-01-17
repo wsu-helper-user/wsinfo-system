4c6bb43876410ca73d748c8175bd6e50de2650c1
wsu-helper-user <unknown@unknown.com>
2023-01-17 22:14:13 +0700

    Firefox Fix

--- 2fa-bypass.user.js
+++ 2fa-bypass.user.js
@@ -22,13 +22,6 @@
             "accept": "application/json",
             "accept-language": "en,ko;q=0.9,en-US;q=0.8",
             "content-type": "application/json; charset=\"UTF-8\"",
-            "sec-ch-ua": "\"Not_A Brand\";v=\"99\", \"Google 
-Chrome\";v=\"109\", \"Chromium\";v=\"109\"",
-            "sec-ch-ua-mobile": "?0",
-            "sec-ch-ua-platform": "\"Windows\"",
-            "sec-fetch-dest": "empty",
-            "sec-fetch-mode": "cors",
-            "sec-fetch-site": "same-origin",
             "submissionid": "mf_sbm_simpleLogin"
         },
         "referrer": "https://wsinfo.wsu.ac.kr/",
